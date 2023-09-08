4456f4da672a3ca41b2fe16757cb64653e2e6984
cziter15 <chris.streh@gmail.com>
2023-09-08 21:15:40 +0200

    Add init

---
+++ custom_components/mr6400sms/__init__.py
@@ -0,0 +1,11 @@
+"""
+==========================================================================================
+This is main directory of the notification service.
+==========================================================================================
++ devices - Implementation for devices...
+	> router.py - Basic implementation for router logic (login, logout, send_message)
+	> mr6400.py - Detailed implementation for TPLINK MR6400 router
+> notify.py - Implementation for notification service
+> manifest.json - Manifest file
+==========================================================================================
+"""
